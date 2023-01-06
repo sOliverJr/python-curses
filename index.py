a2e2e2997914b75660ff20eba737f581d055fb97
Oliver Seider <64847610+sOliverJr@users.noreply.github.com>
2023-01-06 10:25:37 +0100

    UI prototype + refactoring

--- index.py
+++ index.py
@@ -1,52 +1,71 @@
 import curses
 import curses.textpad
 
-# Initialisieren
-stdscr = curses.initscr()
-curses.noecho()
-curses.cbreak()
-stdscr.keypad(1)
-curses.start_color()
-curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLUE)
-curses.init_pair(2, curses.COLOR_YELLOW, curses.COLOR_BLACK)
-stdscr.bkgd(curses.color_pair(1))
+
+def init_curses():
+    # Initialisieren
+    stdscr = curses.initscr()
+    curses.noecho()
+    curses.cbreak()
+    stdscr.keypad(1)
+    curses.start_color()
+    curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLUE)
+    curses.init_pair(2, curses.COLOR_YELLOW, curses.COLOR_BLACK)
+    stdscr.bkgd(curses.color_pair(1))
+    stdscr.refresh()
+    return stdscr
+
+
+def refresh_windows():
+    for win in windows:
+        win.refresh()
+
+
+# Init
+stdscr = init_curses()
 maxy, maxx = stdscr.getmaxyx()
-stdscr.refresh()
+windows = []
+
+# Darstellungsfenster Rand
+display_border_win = curses.newwin(maxy - 3, maxx, 0, 0)
+display_border_win.bkgd(curses.color_pair(1))
+display_border_win.border()
+display_border_win.addstr(0, 2, 'Dialogue')
+windows.append(display_border_win)
+
+# Darstellungsfenster Inhalt
+display_win = curses.newwin(maxy - 5, maxx - 2, 1, 1)
+display_win.bkgd(curses.color_pair(1))
+windows.append(display_win)
 
 # Edit-Fenster Rand
-win1 = curses.newwin(3, maxx, maxy - 3, 0)
-win1.bkgd(curses.color_pair(2))
-win1.border()
-win1.refresh()
+input_border_win = curses.newwin(3, maxx, maxy - 3, 0)
+input_border_win.bkgd(curses.color_pair(2))
+input_border_win.border()
+input_border_win.addstr(0, 2, 'Message')
+windows.append(input_border_win)
 
-# Edit-Fenster box
-win3 = curses.newwin(1, maxx, maxy - 2, 1)
-win3.bkgd(curses.color_pair(2))
-
-# Darstellungsfenster
-win2 = curses.newwin(maxy - 3, maxx, 0, 0)
-win2.bkgd(curses.color_pair(1))
-win2.border()
-
-win2.refresh()
+# Edit-Fenster Box
+input_win = curses.newwin(1, maxx - 2, maxy - 2, 1)
+input_win.bkgd(curses.color_pair(2))
+windows.append(input_win)
 
 # Textbox
-textbox = curses.textpad.Textbox(win3)
+textbox = curses.textpad.Textbox(input_win)
 
-i = 1
+refresh_windows()
+i = 0
 while True:
     text = textbox.edit()
 
     # Text übernehmen
-    win2.addstr(i, 1, '[USER] ' + text + '\n')
+    display_win.addstr(i, 0, '[USER] ' + text + '\n')
     i += 1
-    win2.refresh()
 
-    win3.clear()
-    win3.refresh()
+    input_win.clear()
+    refresh_windows()
 
 # Ende
-c = stdscr.getch()
 curses.nocbreak()
 stdscr.keypad(0)
 curses.echo()
